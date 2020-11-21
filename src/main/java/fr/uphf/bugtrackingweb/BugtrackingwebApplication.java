c5e4db0a9388b68522bea70b798e237df3bf1197
StevenHelin <55350189+StevenHelin@users.noreply.github.com>
2020-11-21 10:24:39 +0100

    Initial commit

---
+++ src/main/java/fr/uphf/bugtrackingweb/BugtrackingwebApplication.java
@@ -0,0 +1,13 @@
+package fr.uphf.bugtrackingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugtrackingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugtrackingwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/uphf/bugtrackingweb/BugtrackingwebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uphf.bugtrackingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugtrackingwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
