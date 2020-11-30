e6f4f062d66998b86497f7ddaf10b9e1ba11a3b1
StevenHelin <55350189+StevenHelin@users.noreply.github.com>
2020-11-30 12:00:35 +0100

    correction de la correction de guillaume

--- src/main/java/fr/uphf/bugtrackingweb/controller/CommentaireController.java
+++ src/main/java/fr/uphf/bugtrackingweb/controller/CommentaireController.java
@@ -1,13 +1,22 @@
 package fr.uphf.bugtrackingweb.controller;
 
+import java.util.List;
+
+
 import fr.uphf.bugtrackingweb.Commentaire;
 import fr.uphf.bugtrackingweb.repositories.CommentaireRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@RestController
 public class CommentaireController
 {
     @Autowired
--- src/main/java/fr/uphf/bugtrackingweb/controller/DeveloppeurController.java
+++ src/main/java/fr/uphf/bugtrackingweb/controller/DeveloppeurController.java
@@ -1,13 +1,20 @@
 package fr.uphf.bugtrackingweb.controller;
 
+import java.util.List;
 import fr.uphf.bugtrackingweb.Developpeur;
 import fr.uphf.bugtrackingweb.repositories.DeveloppeurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@RestController
 public class DeveloppeurController
 {
     @Autowired
