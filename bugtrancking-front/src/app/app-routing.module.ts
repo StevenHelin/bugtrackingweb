73681d4f1cd28d07df84a0e3aa83e63526fe4141
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2020-12-07 11:49:16 +0100

    Création de "create-bug" et "create-developpeur"

--- bugtrancking-front/src/app/app-routing.module.ts
+++ bugtrancking-front/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BugListComponent } from './bug-list/bug-list.component';
+import { BugDetailComponent } from './bug-list/bug-detail.component';
+
 
 const routes: Routes = [];
 
