b74ab76f88db292fef1aa8b4171a536b4eed84e8
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2020-12-10 16:59:28 +0100

    Insertion des requêtes dans le service de bug, reste les requêtes pour developpeur

--- bugtrancking-front/src/app/services/bugs.service.ts
+++ bugtrancking-front/src/app/services/bugs.service.ts
@@ -11,14 +11,53 @@ export class BugsService {
 
   constructor(private http: HttpClient) { }
 
-  public getBugsList(): Observable<Bug[]> {
-    return this.http.get<Bug[]>(`${environment.url}/bugs`);
+  public getBug(bug: Bug): Observable<Bug[]>
+  {
+    return this.http.get<Bug[]>(`${environment.url}/bug/{id}`);
   }
 
-  public deleteBug(id: number): Observable<any> {
-    return this.http.delete(`${environment.url}/bugs/${id}`);
+  public getAllBug(): Observable<Bug[]>
+  {
+    return this.http.get<Bug[]>(`${environment.url}/bug`);
   }
-  public createBug(bug: Bug): Observable<Bug> {
-    return this.http.post<Bug>(`${environment.url}/bugs`, bug);
+
+  public deleteBug(id: number): Observable<any>
+  {
+    return this.http.delete(`${environment.url}/bug/{id}`);
+  }
+
+  public createBug(bug: Bug): Observable<Bug[]>
+  {
+    return this.http.post<Bug>(`${environment.url}/bug`, bug);
+  }
+
+  public getBug(bug: Bug): Observable<Bug[]>
+  {
+    return this.http.get<Bug[]>(`${environment.url}/bug/titre`);
+  }
+
+  public getBug(string: String): Observable<Bug[]>
+  {
+    return this.http.get<Bug[]>(`${environment.url}/bug/titre`);
+  }
+
+  public getBugEtat(etat: String): Observable<Bug[]>
+  {
+    return this.http.get<Bug[]>(`${environment.url}/bug/etat`);
+  }
+
+  public getBugByDate(debut: Date, fin: Date): Observable<Bug[]>
+  {
+    return this.http.get<Bug[]>(`${environment.url}/bug/date`);
+  }
+
+  public replaceBugById(): Observable<Bug[]>
+  {
+    return this.http.put<Bug[]>(`${environment.url}/bug/{id}`);
+  }
+
+  public ajoutDev(id: number, iddev: number): Observable<Bug[]>
+  {
+    return this.http.put<Bug[]>(`${environment.url}/bug/{id}/dev/{iddev}`);
   }
 }
